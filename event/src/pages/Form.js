5665bf4db8bc0c95878ccaa2efd0e5eb8aec8f9f
Rob Cowley <robert.cowley@vitagroup.com>
2024-03-22 15:22:14 +0000

    Button text

--- event/src/pages/Form.js
+++ event/src/pages/Form.js
@@ -95,7 +95,7 @@ const Form = () => {
             className="active:bg-blue-600 hover:shadow-lg focus:outline-none px-6 py-3 mb-1 mr-1 text-sm font-bold text-white uppercase transition-all duration-150 ease-linear bg-blue-500 rounded shadow outline-none"
             type="submit"
           >
-            Send a message
+            Submit event
           </button>
         </div>
       )}
